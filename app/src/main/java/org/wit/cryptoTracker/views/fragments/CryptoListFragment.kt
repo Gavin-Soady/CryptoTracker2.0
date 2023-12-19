e43a86c260fa937cda61e5a5dda8b4a52fb0628b
Gavin-Soady <gsoady19@gmail.com>
2023-12-19 23:13:46 +0000

    Finally got navbar and fragments working

---
+++ app/src/main/java/org/wit/cryptoTracker/views/fragments/CryptoListFragment.kt
@@ -0,0 +1,74 @@
+package org.wit.cryptoTracker.views.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ReportFragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.recyclerview.widget.LinearLayoutManager
+import org.wit.cryptoTracker.R
+import org.wit.cryptoTracker.databinding.FragmentReportBinding
+import org.wit.cryptoTracker.main.MainApp
+import org.wit.cryptoTracker.models.CryptoModel
+import org.wit.cryptoTracker.views.cryptoList.CryptoAdapter
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CryptoListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CryptoListFragment : Fragment() {
+    lateinit var app: MainApp
+    private var _fragBinding: FragmentReportBinding? = null
+    private val fragBinding get() = _fragBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        app = activity?.application as MainApp
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _fragBinding = FragmentReportBinding.inflate(inflater, container, false)
+        val root = fragBinding.root
+        activity?.title = "Crypto List"
+
+        fragBinding.recyclerView.setLayoutManager(LinearLayoutManager(activity))
+        fragBinding.recyclerView.adapter = CryptoAdapter(app.cryptos.findAll())
+
+        return root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            ReportFragment().apply {
+                arguments = Bundle().apply { }
+            }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _fragBinding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_cryptolist, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,
+            requireView().findNavController()) || super.onOptionsItemSelected(item)
+    }
+
+}
